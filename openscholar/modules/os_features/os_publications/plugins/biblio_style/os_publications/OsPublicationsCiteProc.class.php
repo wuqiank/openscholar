454c946844a69b1049d02d2bb3c721bb7a7b0db4
RoySegall <roy.segall@gizra.com>
2013-12-24 16:53:36 +0200

    More css and images.

--- openscholar/modules/os_features/os_publications/plugins/biblio_style/os_publications/OsPublicationsCiteProc.class.php
+++ openscholar/modules/os_features/os_publications/plugins/biblio_style/os_publications/OsPublicationsCiteProc.class.php
@@ -14,6 +14,8 @@ class OsPublicationsCiteProc extends BiblioStyleCiteProc {
     if (empty($this->biblio->title_no_url)) {
       // Convert the title to a URL referencing the bilbio.
       $url = entity_uri('biblio', $this->biblio);
+      $url['options']['attributes']['class'][] = 'entry-link';
+
       $citation = str_replace($this->biblio->title, l($this->biblio->title, $url['path'], $url['options']), $output);
     }
 
