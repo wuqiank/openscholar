8f5c64c47b9fc6011b5ffc4f9d5c0f2955de256d
Matt Petrovic <mpetrovic@iq.harvard.edu>
2014-01-14 16:54:29 -0500

    #4939: Use visibility instead of display to hide buttons. Auto collapse on page load.

--- openscholar/modules/os/modules/os_wysiwyg/os_wysiwyg.widget.js
+++ openscholar/modules/os/modules/os_wysiwyg/os_wysiwyg.widget.js
@@ -3,22 +3,49 @@
  */
 (function ($) {
   function wysiwyg_focus(e) {
-    console.log('focus');
-    console.log(e);
+    var base_id = e.currentTarget.name.replace('_ifr', ''),
+      editor = $('#'+base_id+' + .mceEditor table.mceLayout'),
+      height = (parseInt($('#'+base_id).attr('data-maxrows')) * 25);
+
+    editor.css('height', height+'px')
+      .removeClass('os-wysiwyg-collapsed');
+    $('iframe', editor).css('height', height+1+'px')
+
   }
 
-  function wysiwyg_blur(e) {
-    console.log('blur');
-    console.log(e);
+  function wysiwyg_minimize(element) {
+    var base_id = element.name?element.name.replace('_ifr', ''):element.id.replace('_ifr', ''),
+      editor = $('#'+base_id+' + .mceEditor table.mceLayout'),
+      height = (parseInt($('#'+base_id).attr('data-minrows')) * 20);
+
+    editor.css('height', height+'px')
+      .addClass('os-wysiwyg-collapsed');
+    $('iframe', editor).css('height', height+1+'px');
+
   }
 
   Drupal.behaviors.osWysiwygExpandingTextarea = {
     attach: function (ctx) {
-      setTimeout(function () {
-        var edit_id = $('.os-wysiwyg-expandable').attr('id');
-        tinyMCE.editors[edit_id].contentDocument.getElementsByTagName('body')[0].onfocus = wysiwyg_focus;
-        tinyMCE.editors[edit_id].contentDocument.getElementsByTagName('body')[0].onblur = wysiwyg_blur;
-      }, 500);
+
+      function bindHandlers() {
+        $('.os-wysiwyg-expandable', ctx).each(function () {
+          var edit_id = $(this).attr('id');
+          if (typeof tinyMCE.editors[edit_id].contentDocument !== 'undefined') {
+            tinyMCE.editors[edit_id].contentDocument.getElementsByTagName('body')[0].onfocus = wysiwyg_focus;
+            tinyMCE.editors[edit_id].contentDocument.getElementsByTagName('body')[0].onblur = blurHandler;
+            wysiwyg_minimize($('#'+edit_id+' + .mceEditor .mceIframeContainer iframe').get(0));
+          }
+          else {
+            setTimeout(bindHandlers, 500);
+          }
+        });
+      }
+
+      function blurHandler (e) {
+        wysiwyg_minimize(e.currentTarget);
+      }
+
+      setTimeout(bindHandlers, 500);
     }
   }
 })(jQuery);
