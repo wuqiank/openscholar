dd884c078faf8dc41dff7be30ada0e6abb402ec0
Matt Petrovic <mpetrovic@iq.harvard.edu>
2014-01-28 15:49:24 -0500

    #4938: Adding files through dragndrop correctly adds them to the file list.

--- openscholar/modules/custom/media_draggable/js/media_draggable.widget.js
+++ openscholar/modules/custom/media_draggable/js/media_draggable.widget.js
@@ -10,6 +10,7 @@
  */
 
 (function ($) {
+  var template;
 
   function openBrowser() {
 
@@ -23,16 +24,45 @@
 
   }
 
+  function addRow(file) {
+    var str = template.html(),
+      id = template.attr('id'),
+      count = $('.file-list-single').length - 1;
+
+    str = str.replace('template', count);
+    str = str.replace('-1', file.fid);
+    str = str.replace('value="blank"', 'value="'+file.fid+'"');
+    str = str.replace(' blank', ' '+file.filename);
+    id = id.replace('template', count);
+
+    var $row = $(str),
+      icon = $(file.preview).find('img.file-icon');
+    $row.find('img.file-icon').replaceWith(icon);
+    var $wrapper = $row.wrapAll('<div class="file-list-single form-wrapper media-draggable-processed" id="id"></div>').parent();
+    setupRowHandlers.call($wrapper);
+    $('.file-list-single').parent().append($wrapper);
+  }
+
+  function setupRowHandlers() {
+    $('.edit a', this).click(openEdit);
+    $('.remove a', this).click(removeFile);
+  }
+
   Drupal.behaviors.mediaDraggableWidget = {
-    attach: function (ctx) {
+    attach: function (ctx, settings) {
+      if (typeof template == 'undefined') {
+        template = $('.file-list-single[hidden]');
+      }
       $('.field-widget-media-draggable-file .form-type-dragndrop-upload').once('media-draggable', function () {
         $('.droppable-browse-button', this).unbind().click(openBrowser);
       });
 
-      $('.field-widget-media-draggable-file .file-list-single').once('media-draggable', function () {
-        $('.edit a', this).click(openEdit);
-        $('.remove a', this).click(removeFile);
-      });
+      $('.field-widget-media-draggable-file .file-list-single').once('media-draggable', setupRowHandlers);
+
+      if (typeof settings.mediaDraggable != 'undefined' && ctx.prop('tagName') == 'FORM') {
+        addRow(settings.mediaDraggable.newFile);
+        settings.mediaDraggable.newFile = false;
+      }
     }
   };
 })(jQuery);
