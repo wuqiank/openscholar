95152b3eaf8f1f40dbc07f2aa332762428024a60
RoySegall <roy.segall@gizra.com>
2014-02-13 15:29:42 +0200

    Remove the update hoo and improve the script.

--- openscholar/modules/os_features/os_publications/scripts/update_title_field.php
+++ openscholar/modules/os_features/os_publications/scripts/update_title_field.php
@@ -6,34 +6,57 @@
  */
 
 $nid = drush_get_option('nid') ? drush_get_option('nid') : 0;
+$memory_limit = drush_get_option('memory_limit', 500);
 
 $i = 0;
 $batch = 250;
-while ($i < $batch) {
+
+$query = new EntityFieldQuery();
+$max = $query
+  ->entityCondition('entity_type', 'node')
+  ->entityCondition('bundle', 'biblio')
+  ->propertyCondition('nid', $nid, '>=')
+  ->count()
+  ->execute();
+
+while ($i < $max) {
+  // Free up memory.
+  drupal_static_reset();
+
   $query = new EntityFieldQuery();
   $results = $query
     ->entityCondition('entity_type', 'node')
     ->entityCondition('bundle', 'biblio')
     ->propertyCondition('nid', $nid, '>=')
-    ->range($i, $i + $batch)
+    ->range(0, $batch)
     ->execute();
 
   if (empty($results['node'])) {
-   return;
+    return;
   }
 
   // We found items to process.
   $ids = array_keys($results['node']);
   title_field_replacement_init('node', 'biblio', 'title', $ids);
+  $i = $i + 250;
   $nid = end($ids);
-  ++$i;
 
-  if (drupal_is_cli()) {
-    // Display message when running the update via drush.
+  $params = array(
+    '@start' => reset($ids),
+    '@end' => end($ids),
+    '@iterator' => $i,
+    '@max' => $max,
+  );
+  drush_print(dt('Process node from @start to @end. Batch state: @iterator/@max', $params));
+
+
+  if (round(memory_get_usage()/1048576) >= $memory_limit) {
     $params = array(
-      '@start' => reset($ids),
-      '@end' => end($ids),
+      '@memory' => round(memory_get_usage()/1048576),
+      '@max_memory' => memory_get_usage(TRUE)/1048576,
     );
-    drush_log(dt('Replaced title with field instance for publication number @start to @end', $params), 'success');
+
+    drush_log(dt('Stopped before out of memory. Start process from the node ID @nid', array('@nid' => end($ids))), 'error');
+    return;
   }
 }
