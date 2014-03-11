841b0c73dc255afda7b2096a5235934930df67d2
Matt Petrovic <mpetrovic@iq.harvard.edu>
2014-03-11 11:27:07 -0400

    #5308: Move breadcrumbs into main columns element.

--- openscholar/themes/os_basetheme/templates/page.tpl.php
+++ openscholar/themes/os_basetheme/templates/page.tpl.php
@@ -40,7 +40,6 @@
 		  <?php print $menu_bar; ?>
 		  <!--main menu region end-->
 		<?php endif; ?>
-    <?php if ($breadcrumb): print $breadcrumb; endif; ?>
 
     <?php if($help = render($page['help'])): ?>
 		  <!--help region beg-->
@@ -49,6 +48,7 @@
 		<?php endif; ?>
 
 		<div id="columns" class="clearfix">
+      <?php if ($breadcrumb): print $breadcrumb; endif; ?>
 			<div class="hg-container">
 				<div id="content-column" role="main">
 					<div class="content-inner">
