abc634c36ab34e4167babd1e03722cf22ded483b
Roy Segall <roy.segall@gizra.com>
2014-03-19 13:15:23 +0200

    Building the link with interseption.

--- openscholar/modules/os_features/os_reader/js/os_rss_feed_submission.js
+++ openscholar/modules/os_features/os_reader/js/os_rss_feed_submission.js
@@ -6,37 +6,47 @@
 
   Drupal.behaviors.os_rss_feed_submition = {
     attach: function(context, settings) {
-
-      if (settings.submission_handle) {
-        return;
-      }
-
       $('#edit-submit').click(function(event) {
         event.preventDefault();
 
         var baseUrl = settings.url + "/" + settings.pathPrefix + "os_rss_feed_export";
         var links = '';
         var count = 0;
+        var bundles = new Array();
+        var terms = new Array();
+        var query = '';
 
         // Gather all the bundles.
-        $("#edit-os-rss-feed-content-types").find("input:checked").each(function() {
-          links += "<a href='" + baseUrl + "?type=" + $(this).val() + "'>" + $(this).siblings().text() + "</a><br />";
-          count++;
+        $("#edit-os-rss-feed-content-types").find("input:checked").each(function(index) {
+          bundles[index] = $(this).val();
         });
 
         // Gather all the terms.
-        $(".terms-select").find("option:selected").each(function() {
-          links += "<a href='" + baseUrl + "?term=" + $(this).val() + "'>" + $(this).text() + "</a><br />";
-          count++;
+        $(".terms-select").find("option:selected").each(function(index) {
+          terms[index] = $(this).val();
         });
 
-        if (links == '') {
+        // Build the link query argument.
+        if (bundles.length > 0) {
+          query = "?type=" + bundles.join();
+        }
+
+        if (terms.length > 0) {
+          if (bundles.length > 0) {
+            query += "?term=" + terms.join();
+          }
+          else {
+            query += "&term=" + terms.join();
+          }
+        }
+
+        // Build the HTML output.
+        if (query == '') {
           html = '<span class="empty-selection">' + Drupal.t("You must select of the options above.") + '</span>';
         }
         else {
-          // Build the HTML output.
-          var html = "<h4>" + Drupal.formatPlural(count, 'Your feed is ready', 'Your feeds are ready') + "</h4>";
-          html += links;
+          var html = "<h4>" + Drupal.t('Your feed is ready') + "</h4>";
+          html += "<a href='" + baseUrl + query + "'>" + Drupal.t("Click here") + "</a><br />";
         }
 
         $("#os_rss_feed_results").html(html).attr('class', 'os_rss_fees_results');
