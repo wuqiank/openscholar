7e9b8d4be8a6b6a818b3d8fcb3d29b19c593eaf0
Roy Segall <roy.segall@gizra.com>
2014-04-02 15:11:50 +0300

    Rename the name script and push the WIP script.

--- openscholar/modules/os/modules/os_activity/scripts/update_message_arguments.php
+++ openscholar/modules/os/modules/os_activity/scripts/update_message_arguments.php
@@ -14,11 +14,12 @@
  *
  *  @example:
  *
- *    drush scr hardcoding_the_message_arguments.php --id=30 --batch=450 --memory_limit=4000
+ *    drush scr update_message_arguments.php --id=30 --batch=450 --memory_limit=4000
  */
 $batch = drush_get_option('batch', 250);
 $memory_limit = drush_get_option('memory_limit', 500);
 $id = drush_get_option('id', 1);
+drush_print_r($id);
 
 // Count how much messages we have.
 $query = new EntityFieldQuery();
@@ -46,7 +47,7 @@ while ($max > $i) {
   }
 
   // Load the messages for the current batch.
-  $messages = message_load_multiple($result['message']);
+  $messages = message_load_multiple(array_keys($result['message']));
 
   foreach ($messages as $message) {
     $wrapper = entity_metadata_wrapper('message', $message);
@@ -56,16 +57,15 @@ while ($max > $i) {
 
     // Add the !title argument.
     $allowed_html_elements = variable_get('html_title_allowed_elements', array('em', 'sub', 'sup'));
-    $message->arguments['!title'] = !empty($wrapper->field_node_ref->title_field) ? filter_xss($wrapper->field_node_ref->title_field->value->value(), $allowed_html_elements) : $wrapper->field_node_ref->label();
+    $message->arguments['!title'] = !empty($wrapper->field_node_reference->title_field) ? filter_xss($wrapper->field_node_reference->title_field->value->value(), $allowed_html_elements) : $wrapper->field_node_reference->label();
 
     $save_message = TRUE;
 
     if ($save_message) {
       $param = array(
         '@mid' => $message->mid,
-        '@tokens' => implode(' ', $tokens),
       );
-      drush_log(dt($message->mid . '\ ' . $max . ') Processing the @tokens in the message @mid', $param), 'success');
+      drush_log(dt($message->mid . '\ ' . $max . ') Processing the tokens in the message @mid', $param), 'success');
 
       // Saving the message and the display message for the user.
       message_save($message);
