0aa2749829de1e75071f74ae176a21a2edabcefd
Shushu Inbar <shushu@gizra.com>
2014-04-10 10:25:21 +0300

    WIP, need to solve unchecking (still sends value).

--- openscholar/modules/os_features/os_publications/js/os_publications.js
+++ openscholar/modules/os_features/os_publications/js/os_publications.js
@@ -1,17 +1,22 @@
 (function ($) {
   Drupal.behaviors.os_publications = {
     attach: function(context, settings) {
-      var coded = $('#edit-biblio-year-coded');
-      $('#edit-biblio-year').change(function(){
-        alert("Hello");
-        if (this.getVal != '') {
-          $('.form-item-biblio-year-coded').each(function () {
+      var coded = $("input[name='biblio_year_coded']");
+      var year_field = $('#edit-biblio-year');
+      year_field.change(function() {
+        if (this.value != '') {
+          // Uncheck all radio buttons.
+          coded.each(function () {
             $(this).prop('checked', false);
           });
         }
       });
-//      coded.onChange();
-
+      coded.change(function() {
+        if (this.value != '') {
+          // Empty year field.
+          year_field[0].value = '';
+        }
+      });
     }
   };
 }(jQuery));
