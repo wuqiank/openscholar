9e83de0b48feee9b61d4167b0dab78356ed6d200
Amitaibu <amitai@gizra.com>
2014-04-22 14:29:44 +0300

    Fix JS.

--- openscholar/modules/os/modules/os_taxonomy/os_taxonomy.js
+++ openscholar/modules/os/modules/os_taxonomy/os_taxonomy.js
@@ -8,4 +8,18 @@
     }
   };
 
+  Drupal.behaviors.treeTaxonomyReference = {
+    attach: function () {
+      $('.field-name-og-vocabulary .toggle-wrapper a.expand').click(function(event) {
+        $(this).parents('.item-list').next('.form-type-checkbox-tree').find('.term-reference-tree-button.term-reference-tree-collapsed').trigger('click');
+        event.preventDefault();
+      });
+
+      $('.field-name-og-vocabulary .toggle-wrapper a.collapse').click(function(event) {
+        $(this).parents('.item-list').next('.form-type-checkbox-tree').find('.term-reference-tree-button:not(.term-reference-tree-collapsed)').trigger('click');
+        event.preventDefault();
+      })
+    }
+  };
+
 })(jQuery);
