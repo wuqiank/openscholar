443379f4e9e2f957defe0c182ebd0ab90a9a0819
Roy Segall <roy.segall@gizra.com>
2014-04-27 10:15:35 +0300

    Fix the structure of the HTML for the tpl file.

--- openscholar/modules/os_features/os_publications/templates/dnd-upload-droppable-area-image.tpl.php
+++ openscholar/modules/os_features/os_publications/templates/dnd-upload-droppable-area-image.tpl.php
@@ -7,32 +7,6 @@
 
 $icon = theme('image', array('path' => drupal_get_path('module', 'os_publications') . '/misc/application-pdf.png'));
 $publication_image = theme('image', array('path' => drupal_get_path('module', 'os_publications') . '/misc/publication-cover.png'));
-
-$display_example = '
-  <div class="biblio-dummy-wrapper">
-    <div class="biblio-dummy-image">'
-  . $publication_image .
-  '</div>
-  <div class="biblio-dummy-body">
-    Wand, Jonathan, Gary King and Olivia Lau. 2011.
-    <a>Anchors: Software for Anchoring Vigenttes Data.</a>
-    <em>Jornal of Statistical Software</em>
-    <span>42, no. 3: 1-25.</span>
-  </div>
-  <div class="biblio-dummy-links">
-    <a>Website</a>
-    <a class="biblio-abstract-dummy-link">Abstract</a>
-    <span>
-      <span>'
-  . $icon .
-  '<a>Article</a>
-</span>
-</span>
-</div>
-</div>
-';
-
-$popup_markup = '<a href="#" class="biblio-pop os-publications-image-help" data-popbox="pop2"><span>Help</span></a><span id="pop2" class="biblio-stylebox2">' . $display_example . '</span>';
 ?>
 
 <div class="droppable droppable-image" id="<?php print $element['#dnd_id']; ?>">
@@ -46,13 +20,34 @@ $popup_markup = '<a href="#" class="biblio-pop os-publications-image-help" data-
     <span><?php print render($element['#text']); ?></span>
 
     <?php if ($element['#standard_upload']): ?>
-    <div class="droppable-standard-upload">
-      <span><?php print t('or'); ?></span>
-      <a href="#" class="droppable-browse-button button">
-        <?php print t('Browse'); ?>
-      </a>
-      <?php print $popup_markup; ?>
-    </div>
+      <div class="droppable-standard-upload">
+        <span><?php print t('or'); ?></span>
+        <a href="#" class="droppable-browse-button button">
+          <?php print t('Browse'); ?>
+        </a>
+        <a href="#" class="biblio-pop os-publications-image-help" data-popbox="pop2"><span><?php print t('Help'); ?></span></a>
+        <span id="pop2" class="biblio-stylebox2">
+          <div class="biblio-dummy-wrapper">
+            <div class="biblio-dummy-image"><?php print $publication_image; ?></div>
+            <div class="biblio-dummy-body">
+              Wand, Jonathan, Gary King and Olivia Lau. 2011.
+              <a>Anchors: Software for Anchoring Vigenttes Data.</a>
+              <em>Jornal of Statistical Software</em>
+              <span>42, no. 3: 1-25.</span>
+            </div>
+            <div class="biblio-dummy-links">
+              <a>Website</a>
+              <a class="biblio-abstract-dummy-link">Abstract</a>
+              <span>
+                <span>
+                  <?php print $icon; ?>
+                  <a>Article</a>
+                </span>
+              </span>
+            </div>
+          </div>
+        </span>
+      </div>
     <?php endif ?>
   </div>
 </div>
