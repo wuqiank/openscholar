a91ff778eeee132121a345baaa00e0b9288f6f22
Roy Segall <roy.segall@gizra.com>
2014-04-27 11:44:46 +0300

    Using tpl for unify the way we display the preview of the publicatoin.

--- openscholar/modules/os_features/os_publications/templates/dnd-upload-droppable-area-image.tpl.php
+++ openscholar/modules/os_features/os_publications/templates/dnd-upload-droppable-area-image.tpl.php
@@ -5,8 +5,7 @@
  * Template file for a droppable area of 'dragndrop_upload_widget_image' widget.
  */
 
-$icon = theme('image', array('path' => drupal_get_path('module', 'os_publications') . '/misc/application-pdf.png'));
-$publication_image = theme('image', array('path' => drupal_get_path('module', 'os_publications') . '/misc/publication-cover.png'));
+$preview = theme('os_publications_preview');
 ?>
 
 <div class="droppable droppable-image" id="<?php print $element['#dnd_id']; ?>">
@@ -25,28 +24,7 @@ $publication_image = theme('image', array('path' => drupal_get_path('module', 'o
         <a href="#" class="droppable-browse-button button">
           <?php print t('Browse'); ?>
         </a>
-        <a href="#" class="biblio-pop os-publications-image-help" data-popbox="pop2"><span><?php print t('Help'); ?></span></a>
-        <span id="pop2" class="biblio-stylebox2">
-          <div class="biblio-dummy-wrapper">
-            <div class="biblio-dummy-image"><?php print $publication_image; ?></div>
-            <div class="biblio-dummy-body">
-              Wand, Jonathan, Gary King and Olivia Lau. 2011.
-              <a>Anchors: Software for Anchoring Vigenttes Data.</a>
-              <em>Jornal of Statistical Software</em>
-              <span>42, no. 3: 1-25.</span>
-            </div>
-            <div class="biblio-dummy-links">
-              <a>Website</a>
-              <a class="biblio-abstract-dummy-link">Abstract</a>
-              <span>
-                <span>
-                  <?php print $icon; ?>
-                  <a>Article</a>
-                </span>
-              </span>
-            </div>
-          </div>
-        </span>
+        <?php print $preview; ?>
       </div>
     <?php endif ?>
   </div>
